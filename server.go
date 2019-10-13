37a3cf8a4a0d3278aa89c3681de7ba7979098506
Turtle <orbitalturtle@protonmail.com>
2019-10-12 20:04:56 -0400

    Set up watchtower server

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "os"
+) 
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "3333"
+    CONN_TYPE = "tcp"
+)
+
+func main() {
+    // Listen for incoming connections.
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    if err != nil {
+        fmt.Println("Error listening:", err.Error())
+        os.Exit(1)
+    }
+    // Close the listener when the application closes.
+    defer l.Close()
+
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+
+    for {
+        // Listen for an incoming connection.
+        conn, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting: ", err.Error())
+            os.Exit(1)
+        }
+        // Handle connections in a new goroutine.
+        go handleRequest(&conn)
+    }
+}
+
+func handleRequest(conn *net.Conn) {
+
+    fmt.Println("Server has received a connection: ", conn)
+
+}
