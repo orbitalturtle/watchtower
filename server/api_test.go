b6eeb4a10b27df2408b2a7aa388b3f467fc2972b
Turtle <orbitalturtle@protonmail.com>
2019-12-04 15:32:34 -0800

    Add initial test for appointment endpoint

--- server/api_test.go
+++ server/api_test.go
@@ -1,46 +1,90 @@
-package main 
+package main
 
 import (
-    "bufio"
-    "encoding/gob"
-    "net"
-    "testing"
-    "time"
+	"bufio"
+	"encoding/gob"
+	"net"
+	"testing"
+	"time"
 )
 
 func initServer() {
-    main()
+	main()
 }
 
 // Test that the init endpoint responds if correct data is passed to it.
-func TestInitEndpoint(t *testing.T) {
-    go initServer()
+func TestAppointmentEndpoint(t *testing.T) {
+	go initServer()
 
-    // Give server time to get set up
-    time.Sleep(time.Second)
+	// Give server time to get set up
+	time.Sleep(time.Second)
 
-    cmd := "/init"
+	cmd := "/appointment"
 
-    init := Wt_init{
-       AcceptedCiphers: []string{"chacha20"},
-       Modes: []Mode{Altruistic},
-       Qos: []string{"accountability"},
-    }
+	encryptedBlob := []byte("BlobTest")
+	authToken := []byte("TokenTest")
+	qos := "Accountability"
 
-    conn, err := net.Dial("tcp", "127.0.0.1:3333")
-    if err != nil {
-	t.Fatalf("Some problem connecting to watchtower: %v", err)
-    }
-    defer conn.Close()
+	appointment := Wt_appointment{
+		// TODO: Replace this with actual locator information
+		Locator:          []byte("LocatorTest"),
+		StartBlock:       606680,
+		EndBlock:         606690,
+		EncryptedBlob:    encryptedBlob,
+		EncryptedBlobLen: uint16(len(encryptedBlob)),
+		AuthToken:        authToken,
+		AuthTokenLen:     uint16(len(authToken)),
+		QosData:          qos,
+		QosLen:           uint16(len(qos)),
+	}
 
-    rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	conn, err := net.Dial("tcp", "127.0.0.1:3333")
+	if err != nil {
+		t.Fatalf("Some problem connecting to watchtower: %v", err)
+	}
+	defer conn.Close()
 
-    rw.Writer.Write([]byte(cmd))
+	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
 
-    // var initBytes bytes.Buffer
-    enc := gob.NewEncoder(rw)
-    err = enc.Encode(init)
-    if err != nil {
-        t.Fatalf("Encoding error: %v", err)
-    }
+	rw.Writer.Write([]byte(cmd))
+
+	// var initBytes bytes.Buffer
+	enc := gob.NewEncoder(rw)
+	err = enc.Encode(appointment)
+	if err != nil {
+		t.Fatalf("Encoding error: %v", err)
+	}
 }
+
+// Test that the init endpoint responds if correct data is passed to it.
+// func TestInitEndpoint(t *testing.T) {
+// 	go initServer()
+//
+// 	// Give server time to get set up
+// 	time.Sleep(time.Second)
+//
+// 	cmd := "/init"
+//
+// 	init := Wt_init{
+// 		AcceptedCiphers: []string{"chacha20"},
+// 		Modes:           []Mode{Altruistic},
+// 		Qos:             []string{"accountability"},
+// 	}
+//
+// 	conn, err := net.Dial("tcp", "127.0.0.1:3333")
+// 	if err != nil {
+// 		t.Fatalf("Some problem connecting to watchtower: %v", err)
+// 	}
+// 	defer conn.Close()
+//
+// 	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+//
+// 	rw.Writer.Write([]byte(cmd))
+//
+// 	// var initBytes bytes.Buffer
+// 	enc := gob.NewEncoder(rw)
+// 	err = enc.Encode(init)
+// 	if err != nil {
+// 		t.Fatalf("Encoding error: %v", err)
+// 	}
+// }
