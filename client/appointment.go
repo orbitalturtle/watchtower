bc522bcc91a1ed01a14e44e206690a43c48597c7
Turtle <orbitalturtle@protonmail.com>
2020-03-17 01:53:26 -0400

    Fix blob encryption errors

--- client/appointment.go
+++ client/appointment.go
@@ -1,22 +1,16 @@
 package client
 
 import (
-        "crypto/cipher"
+        // "crypto/cipher"
         cryptorand "crypto/rand"
+        "encoding/json"
 
         "golang.org/x/crypto/chacha20poly1305"
 )
 
-// TODO: Should be more aligned with object-oriented principles.
-
+// TODO: Figure out what other data is needed here.
 type secretBackupData struct {
-        chanID string
-}
-
-type origAppointment struct {
-        Locator          string 
-        StartBlock       uint64
-        EndBlock         uint64
+        blob []byte
 }
 
 type appointment struct {
@@ -26,30 +20,37 @@ type appointment struct {
         EncryptedBlob    []byte
 }
 
-func buildAppointment(origAppt *originalAppointment) *appointment {
-        // Take secret data and encrypt it
+func (a *appointment) addEncryptedBlob(key string, blobData *secretBackupData) error {
+        // Encrypt secret data.
+        encryptedBlob, err := a.encrypt(key, blobData)
+        if err != nil {
+                return err
+        }
 
-        // Add the encrypted blob to the appointment
+        // Add the encrypted blob to the appointment.
+        a.EncryptedBlob = encryptedBlob
 
-        return appt
+        return nil
 }
 
-func encryptAppointment(key string, data *secretData) []byte, error {
-        // Key is the second half of the tx data...
-        
+// Encrypt appointment before sending it to the watchtower so the watchtower 
+// can't see all of our transaction data.
+// Key sis the second half of the tx id
+func (a *appointment) encrypt(key string, blobData *secretBackupData) ([]byte, error) {
         // Convert data into a byte array so it can be encrypted.
+        apptBytes, _ := json.Marshal(*blobData)
 
         aeadCipher, err := chacha20poly1305.New([]byte(key))
         if err != nil {
                return nil, err
         }
 
-        // Encryption
+        // Encrypt data so that only the second half of the tx data can open it.
         nonce := make([]byte, chacha20poly1305.NonceSize)
         if _, err := cryptorand.Read(nonce); err != nil {
             panic(err)
         }
-        encryptedBlob := aead.Seal(nil, nonce, []byte(data), nil)
+        encryptedBlob := aeadCipher.Seal(nil, nonce, apptBytes, nil)
 
         return encryptedBlob, nil
 }
